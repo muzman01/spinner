3287c76c0659d32f0f2411c7d4c70b4f25dbe55c
muzman01 <supurge_man@hotmail.com>
2022-04-18 11:33:41 +0300

    aa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import "./App.css"
+export default class App extends Component {
+  state={
+    name:"circle"
+  }
+  startRotation = () =>{
+    this.setState({
+      name:"circle start-rotate"
+    })
+    setTimeout(()=>{
+      this.setState(
+     {
+      name:"circle start-rotate stop-rotate"
+     }
+      )
+    },Math.floor(Math.random() * 10000) +1)
+  }
+  render() {
+    return (
+      <div>
+        <div className='arrow'></div>
+        <ul className={this.state.name}>
+          <li>
+            <div className='text' contentEditable="true" spellCheck="false">1</div>
+          </li>
+          <li>
+            <div className='text' contentEditable="true" spellCheck="false">2</div>
+          </li>
+          <li>
+            <div className='text' contentEditable="true" spellCheck="false">3</div>
+          </li>
+          <li>
+            <div className='text' contentEditable="true" spellCheck="false">4</div>
+          </li>
+          <li>
+            <div className='text' contentEditable="true" spellCheck="false">5</div>
+          </li>
+          <li>
+            <div className='text' contentEditable="true" spellCheck="false">6</div>
+          </li>
+          <li>
+            <div className='text' contentEditable="true" spellCheck="false">7</div>
+          </li>
+          <li>
+            <div className='text' contentEditable="true" spellCheck="false">8</div>
+          </li>
+          <li>
+            <div className='text' contentEditable="true" spellCheck="false">9</div>
+          </li>
+          <li>
+            <div className='text' contentEditable="true" spellCheck="false">10</div>
+          </li>
+          <li>
+            <div className='text' contentEditable="true" spellCheck="false">11</div>
+          </li>
+          <li>
+            <div className='text' contentEditable="true" spellCheck="false">12</div>
+          </li>
+        </ul>
+        <button className='spin-btn' onClick={this.startRotation}>Çevir</button>
+      </div>
+    )
+  }
 }
 
-export default App;
